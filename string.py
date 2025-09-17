189671b0649a08df04732c9d6181c56b46357b04
alessandraluiza <aleluizasg@gmail.com>
2025-09-17 19:56:09 -0300

    Meu primeiro commit

---
+++ string.py
@@ -0,0 +1,11 @@
+myString = "Isso é uma string"
+print(myString)
+print(type(myString))
+print(myString + " do tipo de dado " + str(type(myString)))
+
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+name = input("Qual é o seu nome ? ")
