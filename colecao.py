94aa6bb970cd1b7d80cb28b0470787a2754a4cca
alessandraluiza <aleluizasg@gmail.com>
2025-09-18 10:29:26 -0300

    atualização

--- colecao.py
+++ colecao.py
@@ -6,3 +6,23 @@ print(listaFrutas[1])
 print(listaFrutas[2])
 listaFrutas[0] = "abacaxi"
 print("Minha lista de frutas alterada: ", listaFrutas)
+
+tuplaFrutas = ("banana", "melancia", "uva")
+print("A minha TUPLA de frutas é: ", tuplaFrutas)
+print("O tipo de dados da minha TUPLA é: ", type(tuplaFrutas))
+print(tuplaFrutas[0])
+print(tuplaFrutas[1])
+print(tuplaFrutas[2])
+
+dicioFrutas = {
+    "Ricardo": "Limão",
+    "Gustavo": "Uva",
+    "Taciara": "Tangerina",
+    "Carol": "Manga"
+}
+print("Meu dicionário de Pessoas e frutas preferidas é: ", dicioFrutas)
+print(type(dicioFrutas))
+print("A Carol gosta de", dicioFrutas["Carol"])
+print(dicioFrutas["Gustavo"])
+print(dicioFrutas["Ricardo"])
+print(dicioFrutas["Taciara"])
