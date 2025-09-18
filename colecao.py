ed279a1851eca165a12294316fd16b53e97c790d
alessandraluiza <aleluizasg@gmail.com>
2025-09-18 09:51:12 -0300

    atualização

--- colecao.py
+++ colecao.py
@@ -1,6 +1,8 @@
 listaFrutas = ["banana", "melância", "uva"]
-print(listaFrutas)
+print("Minha lista de frutas original: ", listaFrutas)
 print(type(listaFrutas))
 print(listaFrutas[0])
 print(listaFrutas[1])
 print(listaFrutas[2])
+listaFrutas[0] = "abacaxi"
+print("Minha lista de frutas alterada: ", listaFrutas)
