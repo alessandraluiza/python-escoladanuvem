afb7a89bb5be1616b110e5181ce10a91f88e5076
alessandraluiza <aleluizasg@gmail.com>
2025-09-18 13:34:19 -0300

    utilizando if, elif, else

---
+++ pacotes.py
@@ -0,0 +1,5 @@
+userReply = input("Você precisa enviar um pacote ? (Digite SIM ou NÃO)")
+if userReply == "SIM":
+    print("Podemos ajuda-lo a enviar este pacote.")
+else:
+    print("Por favor, volte quando você precisar enviar um pacote.")
