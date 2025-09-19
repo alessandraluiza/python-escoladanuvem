835d17fe576f44032a9340a29d6127f2afd2dd85
alessandraluiza <aleluizasg@gmail.com>
2025-09-19 15:52:52 -0300

    Função

---
+++ funcao.py
@@ -0,0 +1,12 @@
+def mensagem():
+    print("Olá mundo!")
+mensagem()
+
+def mensagem2():
+    print("Seja bem vindo")
+mensagem2()
+
+def soma(a, b):
+    return a + b
+resultado = soma(5, 3)
+print(resultado)
