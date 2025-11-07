2d5ff9ea6e71d8c47f43fa64fbccd3b89fde64db
alessandraluiza <aleluizasg@gmail.com>
2025-11-07 17:32:48 -0300

    Exercicio 1

---
+++ Exercicios1/calculadoraPreco.py
@@ -0,0 +1,17 @@
+#4- Calculadora de Preço Total
+#* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+#* Nome do produto: "Cadeira Infantil"
+#* Preço unitário: R$ 12.40
+#* Quantidade: 3
+#O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+
+print("___Calculadora de Preço Total___")
+print("Nome do produto: Cadeira Infantil \nPreço unitário: R$ 12.40 \nQuantidade: 3")
+
+preco_unitario = 12.40
+quantidade = 3
+calculo = preco_unitario*quantidade
+
+
+print(f"Total: {calculo}")
